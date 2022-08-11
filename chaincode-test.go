6c77d25b22721b8fb5d0cabc3c77ba5a0a4d0999
Joseph Shu <86043952+Shumay14@users.noreply.github.com>
2022-08-11 16:38:49 +0900

    .

---
+++ chaincode-test.go
@@ -0,0 +1,18 @@
+// ReadAsset reads the information from collection
+func (s *SmartContract) ReadAsset(ctx contractapi.TransactionContextInterface, assetID string) (*Asset, error) {
+
+	}
+
+
+// ReadAssetPrivateDetails reads the asset private details in organization specific collection
+func (s *SmartContract)	
+
+
+//
+
+
+func (s *SmartContract) CreateAsset(ctx contractapi.TransactionContextInterface, ) error {
+
+	// Get new asset from transient map
+	transientMap, err := ctx.GetStub().
+}
