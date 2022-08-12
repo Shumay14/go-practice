93edfd0455a795cd44393e1278dd9b080fc92b98
Joseph Shu <86043952+Shumay14@users.noreply.github.com>
2022-08-12 11:10:32 +0900

    .

--- chaincode-test.go
+++ chaincode-test.go
@@ -14,5 +14,21 @@ func (s *SmartContract)
 func (s *SmartContract) CreateAsset(ctx contractapi.TransactionContextInterface, ) error {
 
 	// Get new asset from transient map
-	transientMap, err := ctx.GetStub().
+	transientMap, err := ctx.GetStub().GetTransient()
+	if err != nil {
+		return fmt.Errorf("error getting transient: %v", err)
+	}
+
+	// Asset properties are private, therefore they get passed in transient field, instead of func args
+	transientAssetJSON, ok := 
+
+
+	type assetTransientInput struct {
+		Type 		   string
+		ID			   string	
+		Colour		   string
+		Size		   int
+		AppraisedValue int
+
+	}
 }
